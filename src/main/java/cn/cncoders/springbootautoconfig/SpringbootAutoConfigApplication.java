b181181c1f795ac32ce2f41c8e90e6de2e276b12
EricWord <2635863525@qq.com>
2019-06-12 13:14:21 +0800

    Initial commit

---
+++ src/main/java/cn/cncoders/springbootautoconfig/SpringbootAutoConfigApplication.java
@@ -0,0 +1,13 @@
+package cn.cncoders.springbootautoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAutoConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cncoders/springbootautoconfig/SpringbootAutoConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cncoders.springbootautoconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootAutoConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
